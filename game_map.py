e99023528ff2029b2933c338acdbe1eed4aadf59
Pupienus <thscully@gmail.com>
2021-07-12 22:44:03 -0700

    how does this work?

--- game_map.py
+++ game_map.py
@@ -9,9 +9,22 @@ class GameMap:
         self.width, self.height = width, height
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
 
+        self.visible = np.full((width, height), fill_value=False, order="F")
+        self.explored = np.full((width, height), fill_value=False, order="F")
+
     def in_bounds(self, x: int, y: int) -> bool:
         """return True if x and y are within the map"""
         return 0 <= x < self.width and 0 <= y < self.height
 
     def render(self, console: Console) -> None:
-        console.tiles_rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+
+        """
+        Renders the map. If a tile is in the "visible" array, draw with "light" colors
+        Else if in "explored" array, draw with "dark" colors.
+        Else default to "SHROUD"
+        """
+        console.tiles_rgb[0:self.width, 0:self.height] = np.select(
+            condlist=[self.visible, self.explored],
+            choicelist=[self.tiles["light"], self.tiles["dark"]],
+            default=tile_types.SHROUD
+        )
